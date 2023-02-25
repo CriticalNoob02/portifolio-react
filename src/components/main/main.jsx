a9d4d0c03b508ebc39be92a4ebe6fc73f565f865
AdrielDevelop <adrieldchavea20@gmail.com>
2023-02-25 16:34:17 -0300

    feat: create main component

---
+++ src/components/main/main.jsx
@@ -0,0 +1,12 @@
+import '../../style/components/main/main.sass'
+import Summary from './summary'
+
+function Main() {
+    return(
+        <div className="mainBox">
+            <Summary/>
+        </div>
+    )
+}
+
+export default Main
---
+++ src/components/main/summary.jsx
@@ -0,0 +1,9 @@
+import '../../style/components/main/summary.sass'
+
+function Summary() {
+    return(
+        <div className="summary">
+        </div>
+    )
+}
+export default Summary
