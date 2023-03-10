0b69ad4135b2a3a0b9c893ea482bd7f1d7adeb63
AdrielDevelop <adrieldchavea20@gmail.com>
2023-03-10 12:43:22 -0300

    feat: adjusting cards
    Inserting a div with gradient, changing the text position and removing the background of the card

--- src/components/body/cards/cards.jsx
+++ src/components/body/cards/cards.jsx
@@ -4,9 +4,13 @@ function Card(props) {
     return Array.from({length: 5}, (v, k) => k)
         .map( (i) => (
             [<div className={`card${i+1}`}>
-                <h1>{props.title[i]}</h1>
-                <h6>{props.text[i]}</h6>
                 <img src={props.img[i]} className='imgCard' alt='Foto-Perfil' />
+                <div className="gradienteBackground">
+                    <div className="textCard">
+                        <h1>{props.title[i]}</h1>
+                        <p>{props.text[i]}</p>
+                    </div>
+                </div>
             </div>]
             ))
 }
