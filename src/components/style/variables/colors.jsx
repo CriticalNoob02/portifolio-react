ac9af74c7bd5aa0321de464b50562b6fda777f01
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-03-22 17:59:48 -0300

    feat: creat color component in jsx

---
+++ src/components/style/variables/colors.jsx
@@ -0,0 +1,14 @@
+const colors = {
+    primary_dark: '#ffb950',
+    secundary_dark: '#84d994',
+    tertiary_dark: '#ffb3b3',
+    backgroun_dark: '#121212',
+    surface_dark: '#201a1b',
+    primary_light: '#825500',
+    secundary_light: '#126d34',
+    tertiary_light: '#b52234',
+    backgroun_light: '#fffbff',
+    surface_light: '#fffbff',
+}
+
+export default colors
