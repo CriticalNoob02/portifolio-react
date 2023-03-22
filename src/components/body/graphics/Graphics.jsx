f502b10bee9c2e6d4ef1941ef7b270fd5fee055b
AdrielDevelop <adrieldchavea20@gmail.com>
2023-03-21 22:36:01 -0300

    feat: add graphic structure

--- src/components/body/graphics/Graphics.jsx
+++ src/components/body/graphics/Graphics.jsx
@@ -1,9 +1,49 @@
 import "./graphics.sass"
+import { Pie } from "react-chartjs-2";
+import { Chart } from 'chart.js/auto'
 
-function Graphics(props){
-    return(
-        <div>
+function PieChart() {
+    const data = {
+      labels: ["Red", "Blue", "Yellow"],
+      datasets: [
+        {
+          label: "Label do Toolip",
+          data: [12, 19, 3],
+          backgroundColor: ["red", "blue", "yellow"],
+          borderWidth: 1,
+        },
+      ],
+    };
+    const options = {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'bottom',
+          },
+          title: {
+            display: true,
+            text: 'Chart.js Doughnut Chart',
+            color: '#fff',
+            font:{
+                size: 15,
+                family: "'Poppins', sans-serif"
+            },
             
+          },
+        },
+      };
+    return (
+      <div>
+        <Pie data={data} options={options} />
+      </div>
+    );
+  }
+
+
+function Graphics(props){ 
+    return(
+        <div className="graphicsBox">
+            <PieChart/>
         </div>
     )
 }
