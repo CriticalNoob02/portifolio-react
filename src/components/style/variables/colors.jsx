a6f6ccee71ddbbd8841e8c037605f1d3fb7a8a50
AdrielDevelop <adrieldchavea20@gmail.com>
2023-03-22 21:27:50 -0300

    feat: add more colors to use in different environments

--- src/components/style/variables/colors.jsx
+++ src/components/style/variables/colors.jsx
@@ -1,14 +1,29 @@
 const colors = {
-    primary_dark: '#ffb950',
-    secundary_dark: '#84d994',
-    tertiary_dark: '#ffb3b3',
-    backgroun_dark: '#121212',
-    surface_dark: '#201a1b',
-    primary_light: '#825500',
-    secundary_light: '#126d34',
-    tertiary_light: '#b52234',
-    backgroun_light: '#fffbff',
-    surface_light: '#fffbff',
+    colorThema: {
+        primary_dark: '#ffb950',
+        secundary_dark: '#84d994',
+        tertiary_dark: '#ffb3b3',
+        backgroun_dark: '#121212',
+        surface_dark: '#201a1b',
+        primary_light: '#825500',
+        secundary_light: '#126d34',
+        tertiary_light: '#b52234',
+        backgroun_light: '#fffbff',
+        surface_light: '#fffbff',
+    },
+    colorDefault:{
+        color1: '#6D435A',
+        color2: '#424B54',
+        color3: '#ffb950',
+        color4: '#ED254E',
+        color5: '#04773B',
+        color6: '#C0B9DD',
+        color7: '#511730',
+        color8: '#84d994',
+        color9: '#50514F',
+        color10: '#1C0221',
+        color11: '#1BE7FF',
+    }
 }
 
 export default colors
