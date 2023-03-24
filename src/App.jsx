1e17fc9b676d94a63d66793dfe37196e700c7235
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-03-24 17:32:17 -0300

    refactory: modifying base github request location

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './components/style/main.sass'
 import React, { useEffect, useState } from 'react'
-import GithubEndpoint from './service/client'
+import requestData from './service/setGithubDataBase'
 import Header from './components/header/header'
 import Footer from './components/footer/footer'
 import Avatar from './assets/Avatar.jpg'
@@ -12,20 +12,11 @@ function App() {
     //Função que ira guardar os dados da requisição
     const [data, setData] = useState([])
 
-    //Função para recuperar as informações da API
-    async function getBaseGithub(){
-        try {
-            const response = await GithubEndpoint.get(`/CriticalNoob02`)
-            const data = response.data
-            setData(data)
-        } catch (error) {
-            console.log("Erro ao recuperar informações da API \n" + error)
-        }
-    }
-
     //Função para gerar um estado logo quando é carregado o app
     useEffect(() => {
-        getBaseGithub()
+        Promise.resolve(requestData)
+        .then( res => setData(res))
+        .catch(erro => console.log(erro))
     }, [])
 
     return (
