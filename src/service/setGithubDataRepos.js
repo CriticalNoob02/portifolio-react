04bba843b512d96f57b020c3aa6894809de72049
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-03-29 16:23:09 -0300

    refactory: transform data in object

--- src/service/setGithubDataRepos.js
+++ src/service/setGithubDataRepos.js
@@ -6,11 +6,18 @@ const requestRepos = fetch(`${URLgit}/CriticalNoob02/repos`)
     let contador = 0
     const repos = resData.map((line) =>{ 
         const { name, html_url, language } = line
-        const repositories = [contador, name, html_url, language]
+        const repositories = {
+            id:contador,
+            name:name, 
+            link:html_url, 
+            lang:language
+        }
         contador++
         return repositories
      })
     return Promise.all(repos)
 })
 
-export default requestRepos
+const resqu = Promise.resolve(requestRepos)
+console.log(resqu)
+export default resqu
