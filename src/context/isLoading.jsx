b9e1d3affc8d2753aa56c58670e50691c9222fed
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-04-24 14:55:04 -0300

    feat: creat context loading

---
+++ src/context/isLoading.jsx
@@ -0,0 +1,16 @@
+import { createContext, useState } from "react";
+
+export const isLoading = createContext({})
+
+export const Loading = ({ children }) => {
+    const [loading1, setLoading1] = useState(true)
+    const [loading2, setLoading2] = useState(true)
+    const [loading3, setLoading3] = useState(true)
+
+    return(
+        <isLoading.Provider value={{loading1, setLoading1, loading2, setLoading2, loading3, setLoading3}} >
+            { children }
+        </isLoading.Provider>
+    )
+}
+
