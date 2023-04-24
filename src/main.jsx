aff3c5ec2e46f3f21c632a88aefe587d33a2438f
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-04-24 14:53:51 -0300

    feat: creat and implement Flex element

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import Base from './components/Flex/Base'
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Base children={<App />} />
   </React.StrictMode>,
 )
