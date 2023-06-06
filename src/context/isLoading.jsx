f3873a718b0e1fff09f30dfbea4d15cfc8fcd0db
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-06-06 14:22:42 -0300

    feat: new loading state

--- src/context/isLoading.jsx
+++ src/context/isLoading.jsx
@@ -6,9 +6,10 @@ export const Loading = ({ children }) => {
     const [loading1, setLoading1] = useState(true)
     const [loading2, setLoading2] = useState(true)
     const [loading3, setLoading3] = useState(true)
+    const [loading4, setLoading4] = useState(true)
 
     return(
-        <isLoading.Provider value={{loading1, setLoading1, loading2, setLoading2, loading3, setLoading3}} >
+        <isLoading.Provider value={{loading1, setLoading1, loading2, setLoading2, loading3, setLoading3, loading4, setLoading4}} >
             { children }
         </isLoading.Provider>
     )
