247a60fec52fcdb14c6c9493cad136f03d688fe9
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-06-12 11:34:28 -0300

    refactor: change img in card

--- src/feature/body/Body.jsx
+++ src/feature/body/Body.jsx
@@ -7,13 +7,13 @@ import GraphicLine from './graphicsLine/GraphicLine'
 
 import Img1 from '../../assets/img1.jpg'
 import Img2 from '../../assets/img2.png'
-import Img3 from '../../assets/img3.png'
+import Img3 from '../../assets/img3.jpg'
 import Img4 from '../../assets/img4.png'
 import Img5 from '../../assets/img5.png'
 
 function Body(){
-    const titlesCards = ['Charts.js','Sass','Algo','algo','Algo2']
-    const textsCards = ['Documentação','Documentação','Algo2','algoo','Algo2']
+    const titlesCards = ['Charts.js','Sass','Github','algo','Algo2']
+    const textsCards = ['Documentação','Documentação','Documentação','algoo','Algo2']
     const imgsCards = [Img1,Img2,Img3,Img4,Img5]
 
     return(
