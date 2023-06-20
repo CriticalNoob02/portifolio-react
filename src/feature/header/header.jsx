8676ccc706210d8dd6c136ec0a7636a379feb1f2
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-06-20 13:14:10 -0300

    feat: apply themes in header

--- src/feature/header/header.jsx
+++ src/feature/header/header.jsx
@@ -1,23 +1,23 @@
 import './header.sass'
-import { DiJsBadge,DiReact,DiPython,DiSass } from "react-icons/di"
-
+import React ,{ useContext } from "react"
+import ButtonTheme from '../../components/buttons/ButtonTheme'
+import { DARK_THEME, LIGHT_THEME, ThemeContext } from '../../context/ThemeParent'
 
 function Header(props) {
+
+    const { globalTheme } = useContext(ThemeContext)
+
     return (
-        <div className="headerBox">
+        <div className={`headerBox ${globalTheme}`}>
             <div className='avatar'>
                 <div className='color'></div>
-                <img src={props.foto} className='img' alt='Foto-Perfil' />
+                <img src={props.foto} className={`img ${globalTheme}`} alt='Foto-Perfil' />
             </div>
-            <div className='title'>
+            <div className={`title ${globalTheme}`}>
                 <h1>{props.nome}</h1>
             </div>
-            <div className='icons'>
-                <DiJsBadge className='icon1'/>
-                <DiReact className='icon2'/>
-                <DiPython className='icon3'/>
-                <DiSass className='icon4'/>
-            </div>
+            <ButtonTheme changeTheme={LIGHT_THEME}>Light</ButtonTheme>
+            <ButtonTheme changeTheme={DARK_THEME}>Dark</ButtonTheme>
         </div>
     )
 }
