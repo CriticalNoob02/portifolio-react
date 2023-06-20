a07ed09a32123783b6bbb0e535a23e3a04a8e623
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-06-20 14:57:13 -0300

    feat(#35): apply theme in Footer

--- src/feature/footer/footer.jsx
+++ src/feature/footer/footer.jsx
@@ -1,12 +1,17 @@
 import './footer.sass'
+import { useContext } from "react"
+import { ThemeContext } from '../../context/ThemeParent'
 
 function Footer(props){
+
+    const { globalTheme } = useContext(ThemeContext)
+
     return(
-        <div className="footerBox">
-            <p className='signature'>
-                Criado por <a href={props.link} target="_blank">{props.nome}</a>
+        <div className={`footerBox ${globalTheme}`}>
+            <p>
+                Criado por <a href={props.link} target="_blank" className={`signature ${globalTheme}`}>{props.nome}</a>
             </p>
-            <img src={props.selo} alt="Selo" className='stamp'/>
+            <img src={props.selo} alt="Selo" className={`stamp ${globalTheme}`}/>
         </div>
     )
 }
