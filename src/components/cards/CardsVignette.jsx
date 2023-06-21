7b9664235cb5ef5fffe33229ddafe8de6f5c66ea
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-06-21 13:53:20 -0300

    feat(#35): apply theme  in CardsVignette component

--- src/components/cards/CardsVignette.jsx
+++ src/components/cards/CardsVignette.jsx
@@ -1,12 +1,17 @@
 import "./CardsVignette.sass"
+import { useContext } from "react"
+import { ThemeContext } from '../../context/ThemeParent'
 
 function CardsVignette(props) {
+
+    const { globalTheme } = useContext(ThemeContext)
+
     return Array.from({length: 5}, (v, k) => k)
         .map( (i) => (
-            [<div className={`card${i+1}`} key={i}>
+            [<div className={`card${i+1} ${globalTheme}`} key={i}>
                 <img src={props.img[i]} className='imgCard' alt='Foto-Perfil' />
                 <div className="gradienteBackground">
-                    <div className="textCard">
+                    <div className={`textCard ${globalTheme}`}>
                         <h1>{props.title[i]}</h1>
                         <a href={props.link[i]} target={"_blank"}><p>Documentação</p></a>
                     </div>
--- src/feature/body/cardsInit/cardsInit.jsx
+++ src/feature/body/cardsInit/cardsInit.jsx
@@ -1,10 +1,15 @@
 import "./cardsInit.sass"
 import CardsVignette from "../../../components/cards/CardsVignette"
+import { useContext } from "react"
+import { ThemeContext } from '../../../context/ThemeParent'
 
 function CardsInit(props){
+
+    const { globalTheme } = useContext(ThemeContext)
+
     return(
         <div className="bodySection">
-        <h1 className="titleReposGit">Ferramentas desta aplicação</h1>
+        <h1 className={`titleBody ${globalTheme}`}>Ferramentas desta aplicação</h1>
             <div className='cards'>
                <CardsVignette
                     title={props.title}
