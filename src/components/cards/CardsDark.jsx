b3aa439d7ace2490986c43194a1ba6873777c207
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-06-21 17:30:50 -0300

    feat(#35): apply theme in CardsDark component

--- src/components/cards/CardsDark.jsx
+++ src/components/cards/CardsDark.jsx
@@ -1,17 +1,22 @@
-import ButtonCards from "../buttons/ButtonCards"
-import { Fragment } from "react"
-import { motion } from "framer-motion"
 import "./CardsDark.sass"
+import ButtonCards from "../buttons/ButtonCards"
+import { Fragment, useContext } from "react"
+import { motion } from "framer-motion"
+import { ThemeContext } from '../../context/ThemeParent'
 
 function CardsDark(props) {
+
+    const { globalTheme } = useContext(ThemeContext)
     const obj = props.obj
+
+
     return Array.from({ length: obj.length }, (v, k) => k)
         .map((i) => ([
         <Fragment key={ obj[i].id }>
-            <motion.div className="cardDarkBorder" whileTap={{cursor: "grabbing"}} >
-                <div className="cardDarkBody">
-                    <h1 className="cardDarkTitle">{ obj[i].name }</h1>
-                    <p className="cardDarkParagraph">{ obj[i].language ?? "Readme"}</p>
+            <motion.div className={`cardDarkBorder ${globalTheme}`} whileTap={{cursor: "grabbing"}} >
+                <div className={`cardDarkBody ${globalTheme}`}>
+                    <h1 className={`cardDarkTitle ${globalTheme}`}>{ obj[i].name }</h1>
+                    <p className={`cardDarkParagraph ${globalTheme}`}>{ obj[i].language ?? "Readme"}</p>
                     <ButtonCards
                         title={ props.buttonTitle }
                         link={ obj[i].link }
