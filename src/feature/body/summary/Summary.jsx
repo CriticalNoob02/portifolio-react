d9bbdea8b7f4b5c77721dddeb673190e542a5494
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-06-21 10:10:34 -0300

    refactor(#35): apply theme in Sumary

--- src/feature/body/summary/Summary.jsx
+++ src/feature/body/summary/Summary.jsx
@@ -1,14 +1,19 @@
 import "./summary.sass"
+import { useContext } from "react"
+import { ThemeContext } from '../../../context/ThemeParent'
 
 function Summary(){
+
+    const { globalTheme } = useContext(ThemeContext)
+
     var version = '1.2.0'
     var title = "Bem-vindo ao meu Portifólio!!"
     var text = `Me chamo Adriel e iniciei minha jornada na área da tecnologia em abril/2022, desde então tenho atualizado esse repositório para mostrar tudo o que aprendi nesse tempo, atualmente estou na versão ${version}`
     
     return(
-        <div className="summary">
-            <h1>{title}</h1>
-            <p>{text}</p>
+        <div className={`summary ${globalTheme}`}>
+            <h1 className={`titleBody ${globalTheme}`}>{title}</h1>
+            <p className={`paragraphBody ${globalTheme}`}>{text}</p>
         </div>
     )
 }
