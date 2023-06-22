9f82f0c9bb049dca32e10190a19ffc60a57b0380
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-06-22 17:31:50 -0300

    feat(#35): apply theme in buttonCard component

--- src/components/buttons/ButtonCards.jsx
+++ src/components/buttons/ButtonCards.jsx
@@ -1,11 +1,15 @@
 import "./ButtonCards.sass"
-import { Fragment } from "react"
+import { Fragment, useContext } from "react"
+import { ThemeContext } from '../../context/ThemeParent';
 
 function ButtonCards(props) {
+    
+    const { globalTheme } = useContext(ThemeContext)
+    
     return(
         <Fragment>
             <a href={props.link} target={"_blank"}>
-                <button className="buttonRepos">{props.title}</button>
+                <button className={`buttonRepos ${globalTheme}`}>{props.title}</button>
             </a>
         </Fragment>
     )
