f0cc443615e976d457f9ae148ccf3f67bb4defdf
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-06-22 12:48:06 -0300

    feat(#35): apply theme in title

--- src/feature/body/graphicsLine/GraphicLine.jsx
+++ src/feature/body/graphicsLine/GraphicLine.jsx
@@ -1,23 +1,26 @@
 import LineChart from "../../../components/charts/Linechart"
 import React, { useEffect, useState, Fragment, useContext } from 'react'
+import { ThemeContext } from '../../../context/ThemeParent'
 import { isLoading } from "../../../context/isLoading"
 import Loader from "../../../components/loaders/loader"
 import './graphicsLine.scss'
 
 function GraphicLine(){ 
+
+  const { globalTheme } = useContext(ThemeContext)
   const {loading4, setLoading4} = useContext(isLoading)
 
- const [lang, setLang] = useState()
+  const [lang, setLang] = useState()
 
- useEffect(() => {
-    fetch("http://localhost:5100/CriticalNoob02/events")
-    .then( res => res.json() )
-    .then( resData => {
-      setLang(resData)
-      setLoading4(false)
-    })
-    .catch(erro => console.log(erro))
- }, [])
+  useEffect(() => {
+      fetch("http://localhost:5100/CriticalNoob02/events")
+      .then( res => res.json() )
+      .then( resData => {
+        setLang(resData)
+        setLoading4(false)
+      })
+      .catch(erro => console.log(erro))
+  }, [])
  
   // Dados da Api
   const title = 'Eventos no Github'
@@ -30,29 +33,28 @@ function GraphicLine(){
     dataApi.push(lang[key])
   }
 
+  return(
+      <Fragment>
+        <h1 className={`titleBody ${globalTheme}`}>Eventos do perfil</h1>
 
-    return(
-        <Fragment>
-          <h1 className="titleReposGit">Eventos do perfil</h1>
+        {
+        loading4
+          ?
+        <Loader/>
+          :
+        <div className="graphicsBoxLine">
+          <LineChart
+            labelData={labelApi}
+            descriptionData={labelToolip}
+            valueData={ dataApi }
+            color={color}
+            titleText={title}
+          />
+        </div>
+        }
 
-          {
-          loading4
-            ?
-          <Loader/>
-            :
-          <div className="graphicsBoxLine">
-            <LineChart
-              labelData={labelApi}
-              descriptionData={labelToolip}
-              valueData={ dataApi }
-              color={color}
-              titleText={title}
-            />
-          </div>
-          }
-
-        </Fragment>
-    )
+      </Fragment>
+  )
 }
 
 export default GraphicLine
