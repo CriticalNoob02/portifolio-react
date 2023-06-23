e03b5f9b2fdd372d95e48f22019324532c1a7c99
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-06-23 17:47:22 -0300

    refactor(#35): adapt the colors

--- src/components/cards/CardsVignette.jsx
+++ src/components/cards/CardsVignette.jsx
@@ -10,7 +10,7 @@ function CardsVignette(props) {
         .map( (i) => (
             [<div className={`card${i+1} ${globalTheme}`} key={i}>
                 <img src={props.img[i]} className='imgCard' alt='Foto-Perfil' />
-                <div className="gradienteBackground">
+                <div className={`gradienteBackground ${globalTheme}`}>
                     <div className={`textCard ${globalTheme}`}>
                         <h1>{props.title[i]}</h1>
                         <a href={props.link[i]} target={"_blank"}><p>Documentação</p></a>
--- src/components/loaders/loader.jsx
+++ src/components/loaders/loader.jsx
@@ -1,9 +1,14 @@
 import "./loader.sass"
+import { useContext } from "react"
+import { ThemeContext } from '../../context/ThemeParent';
 
 function Loader(props) {
+    
+    const { globalTheme } = useContext(ThemeContext)
+    
     return(
-        <div className="baseSpinner" style={{height: props.height ?? '100%'}}>
-        <div className="spinner"></div>
+        <div className={`baseSpinner ${globalTheme}`} style={{height: props.height ?? '100%'}}>
+        <div className={`spinner ${globalTheme}`}></div>
         </div>
     )
 }
