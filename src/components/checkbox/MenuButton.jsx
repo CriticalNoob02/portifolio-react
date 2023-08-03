549887a00d46bf6383b5b6d8420a116ee62da391
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-08-03 08:28:54 -0300

    test: apply event in button

--- src/components/checkbox/MenuButton.jsx
+++ src/components/checkbox/MenuButton.jsx
@@ -4,7 +4,7 @@ function MenuButton(){
     
     return(
         <label class="burger" for="burger">
-            <input type="checkbox" id="burger"/>
+            <input type="checkbox" id="burger" onChange={ (e) => {console.log(10)} }/>
             <span></span>
             <span></span>
             <span></span>
